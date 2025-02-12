c66998c35ffee4cd3c7ff508c235eaa6a462a25b
Alejandro Ramos Celaya <46717209+Aramoscelaya@users.noreply.github.com>
2025-02-12 13:38:43 -0500

    Add files via upload
    se separan componentes de camara

--- camera.py
+++ camera.py
@@ -1,6 +1,18 @@
 import cv2
-
 import base64
+import threading
+import cv2
+import time
+import pygame
+from scanner import scan_code
+
+scanning = False
+result_text = None
+page = None
+
+pygame.mixer.init()
+sound = pygame.mixer.Sound('beep.mp3')
+
 
 def get_camera():
     cap = cv2.VideoCapture(0)
@@ -17,3 +29,66 @@ def get_frame(cap):
 def encode_frame_to_base64(frame):
     _, buffer = cv2.imencode('.jpg', frame)
     return base64.b64encode(buffer).decode('utf-8')
+
+def start_scan(p, rs, e):
+    global scanning, page, result_text
+    page = p
+    result_text = rs
+    scanning = True
+    threading.Thread(target=(scan_loop)).start()
+
+def scan_loop():
+    cap = get_camera()
+    global scanning
+    code_count = {}
+    frames_checked = 0
+
+    while scanning:
+        frame = get_frame(cap)
+        code = scan_code(frame)
+
+        if code:
+            if code in code_count:
+                code_count[code] += 1
+            else:
+                code_count[code] = 1
+            frames_checked += 1 
+
+            height, width, _ = frame.shape
+            cv2.rectangle(frame, (50, height - 60), 
+                        (width - 50, height - 10), 
+                        (0, 255, 0), -1)
+            cv2.putText(frame, f"Detected: {code}", 
+                        (60, height - 30), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                        (255, 255, 255), 2)
+
+            if frames_checked >= 10:
+                for c, count in code_count.items():
+                    if count >= 10:
+                        result_text.value = f"Resultado: {c}"
+                        getCode(c)
+                        sound.play()
+                        time.sleep(0.5)
+                        break
+
+                code_count.clear()
+                frames_checked = 0
+
+        page.image.src_base64 = encode_frame_to_base64(frame)
+        page.update()
+
+def getCode(code):
+    codes = code[0]
+    datos = codes.split(',')
+    items = []
+
+    #datos = ['id_producto-PF4HD0K', 'hostname-WPHI002']
+    for i in datos:
+        item = {}
+        item["name"] = i.split('-')[0]
+        item["value"] = i.split('-')[1]
+
+        items.append(item)
+    
+    print(items)
--- main.py
+++ main.py
@@ -1,104 +1,20 @@
 import flet as ft
-import threading
-from camera import get_camera, get_frame, encode_frame_to_base64
+#import threading
+from camera import get_camera, get_frame, encode_frame_to_base64, start_scan
 from scanner import scan_code
-import cv2
-import pygame
-import time
+#import cv2
+#import pygame
+#import time
 #from database import guardar_codigo, get_code
+#from products import 
 
 
-cap = get_camera()
-scanning = False
+#scanning = False
 result_text = None
 page = None
 
-pygame.mixer.init()
-sound = pygame.mixer.Sound('beep.mp3')
-
-
-def start_scan(e):
-    global scanning
-    scanning = True
-    threading.Thread(target=scan_loop).start()
-
-def scan_loop():
-    global scanning, page
-    code_count = {}
-    frames_checked = 0
-
-    while scanning:
-        frame = get_frame(cap)
-        code = scan_code(frame)
-
-        if code:
-            if code in code_count:
-                code_count[code] += 1
-            else:
-                code_count[code] = 1
-            frames_checked += 1 
-
-            height, width, _ = frame.shape
-            cv2.rectangle(frame, (50, height - 60), 
-                          (width - 50, height - 10), 
-                          (0, 255, 0), -1)
-            cv2.putText(frame, f"Detected: {code}", 
-                        (60, height - 30), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, 
-                        (255, 255, 255), 2)
-
-            if frames_checked >= 10:
-                for c, count in code_count.items():
-                    if count >= 10:
-                        result_text.value = f"Resultado: {c}"
-                        getCode(c)
-                        sound.play()
-                        time.sleep(0.5)
-                        break
-
-                code_count.clear()
-                frames_checked = 0
-
-        page.image.src_base64 = encode_frame_to_base64(frame)
-        page.update()
-
-def getCode(code):
-    codes = code[0]
-    # Dividir la cadena por comas
-    datos = codes.split(',')
-    items = []
-    
-    #datos = ['id_producto-PF4HD0K', 'hostname-WPHI002']
-    for i in datos:
-        item = {}
-        item["name"] = i.split('-')[0]
-        item["value"] = i.split('-')[1]
-
-        items.append(item)
-    
-    print(items)
-
-def camara_main(p: ft.Page):
-    global page, result_text 
-    page = p 
-    page.title = "Escáner de Códigos"
-    page.window.width = 800
-    page.window.height = 600
-
-    result_text = ft.Text(value="Resultado: ", size=20)
-    #scan_button = ft.ElevatedButton(text="Scan", on_click=start_scan)
-    start_scan(e="")
-
-    camera_image = ft.Image(src="0.png", width=640, height=480)
-    page.image = camera_image
-
-    page.add(
-        ft.Column([
-            camera_image,
-            #scan_button,
-            result_text,
-        ], alignment=ft.MainAxisAlignment.CENTER)
-    )
+#pygame.mixer.init()
+#sound = pygame.mixer.Sound('beep.mp3')
 
 def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
@@ -111,8 +27,6 @@ def main(page: ft.Page):
             home(page)
         elif seccion == "add_items":
             add_items(page)
-            #seccion_activa.value = add_items(page)
-            #menu_btn.icon = ft.Icons.PERSON
         elif seccion == "configuracion":
             conf(page)
         else:
@@ -161,6 +75,31 @@ def main(page: ft.Page):
 
     page.update()
 
+
+
+
+def camara_main(p: ft.Page):
+    global page, result_text 
+    page = p 
+    page.title = "Escáner de Códigos"
+    page.window.width = 800
+    page.window.height = 600
+
+    result_text = ft.Text(value="Resultado: ", size=20)
+    #scan_button = ft.ElevatedButton(text="Scan", on_click=start_scan)
+    start_scan(e="")
+
+    camera_image = ft.Image(src="0.png", width=640, height=480)
+    page.image = camera_image
+
+    page.add(
+        ft.Column([
+            camera_image,
+            #scan_button,
+            result_text,
+        ], alignment=ft.MainAxisAlignment.CENTER)
+    )
+
 def conf(page):
     code = '4026700424256'
     print(code)
@@ -175,7 +114,7 @@ def add_items(p: ft.Page):
 
     result_text = ft.Text(value="Resultado: ", size=20)
     #scan_button = ft.ElevatedButton(text="Scan", on_click=start_scan)
-    start_scan(e="")
+    start_scan(page, result_text, e="")
 
     camera_image = ft.Image(src="0.png", width=640, height=480)
     page.image = camera_image
@@ -223,6 +162,6 @@ def home(page):
     )
 
 
-ft.app(target=main, view=ft.WEB_BROWSER)
+#ft.app(target=main, view=ft.WEB_BROWSER)
 #ft.app(target=main, view=ft.FLET_APP)
-#ft.app(target=main)
+ft.app(target=main)
